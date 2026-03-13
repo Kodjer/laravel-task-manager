8681b60057d885bcf58eea523df9d9d51aa97423
Oleksandr Sazonov <ssazonov228@gmail.com>
2026-03-13 18:20:34 +0200

    Add categories: create table, model and relationships

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -14,9 +14,21 @@ class Task extends Model
         'description',
         'status',
         'due_date',
+        'user_id',
+        'category_id'
     ];
 
     protected $casts = [
         'due_date' => 'date',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
